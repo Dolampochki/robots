a09ba07976b3ba3565ae6500365efb463ae951ba
Katya Dolgov <katya.dolgov@hubsecurity.io>
2022-08-20 21:35:21 +0300

    place setting popup in a right place

--- src/Components/Controllers/SettingsPopup/SettingsPopup.js
+++ src/Components/Controllers/SettingsPopup/SettingsPopup.js
@@ -12,13 +12,59 @@ export const SettingsPopup = ({ updateSettings, part, isOpen, settings, side, nu
     const done = () => {
         updateSettings(currentSettings)
     }
-    console.log(number)
-    const settingPopupWidth = colors.length * 56 + 32
+    
     const settingsPopupElementId = `settings-popup-${part}-${number}${side ? '-' + side : ''}`
     const settingsPopupElement = document.getElementById(settingsPopupElementId)
-    const targetElement = settingsPopupElement?.previousSibling
-    const targetElementCoords = targetElement ? targetElement.getBoundingClientRect() : { top: 0, left: 0, width: 0 }
-    const style = { left: `${targetElementCoords.left + targetElementCoords.width / 2  - settingPopupWidth / 2 - 4}px`, top: `${targetElementCoords.top}px`, width: `${settingPopupWidth}px` }
+    
+    const getStyle = () => {
+        const settingPopupWidth = colors.length * 56 + 32
+        const settingPopupHeight = (settingsForParts[part] ? settingsForParts[part].length : 0) * 60 + 140
+        const targetElement = settingsPopupElement?.previousSibling
+        const targetElementCoords = targetElement ? targetElement.getBoundingClientRect() : { top: 0, left: 0, bottom: 0, width: 0 }
+        let direction = 'bottom'
+        const spacer = 40
+        const arrowSpacer = 8
+
+        const targetElementCenter = targetElementCoords.left + targetElementCoords.width / 2
+        let left = targetElementCenter  - settingPopupWidth / 2
+        let arrowLeft = 0
+        if (left < spacer) {
+            left = spacer
+            arrowLeft = targetElementCenter - arrowSpacer
+        }
+        if (left + settingPopupWidth > window.innerWidth - spacer) {
+            left = window.innerWidth - settingPopupWidth - spacer
+            arrowLeft = targetElementCenter - arrowSpacer
+        }
+
+        let top = targetElementCoords.top + 24
+        if (top + settingPopupHeight > window.innerHeight) {
+            const targetBottomTop = targetElementCoords.bottom ? targetElementCoords.bottom - spacer - settingPopupHeight : 0
+            top = window.innerHeight - settingPopupHeight - spacer
+            if (targetBottomTop && targetBottomTop >= spacer) {
+                top = targetBottomTop
+                direction = 'top'
+            } 
+        }
+        const popupStyle = { 
+            left: `${left}px`, 
+            top: `${top}px`, 
+            width: `${settingPopupWidth}px`,
+            height: `${settingPopupHeight}px` 
+        }
+
+        const arrowTop = direction === 'bottom' ? top - arrowSpacer : top + settingPopupHeight - arrowSpacer
+
+        const arrowStyle = {
+            top: `${arrowTop}px`
+        }
+
+        if (arrowLeft) arrowStyle.left = `${arrowLeft}px`
+
+        return { popupStyle, direction, arrowStyle }
+    }
+
+    const { popupStyle, direction, arrowStyle } = getStyle()
     
     useEffect(() => {
         if (!isSameObject(settings, currentSettings)) setCurrentSettings(settings)
@@ -42,7 +88,8 @@ export const SettingsPopup = ({ updateSettings, part, isOpen, settings, side, nu
     }, [])
 
     return (
-        <div id={settingsPopupElementId} className={`settings-popup ${isOpen ? 'show' : ''}`} style={style}>
+        <div id={settingsPopupElementId} className={`settings-popup ${direction} ${isOpen ? 'show' : ''}`} style={popupStyle}>
+            <div className='settings-popup-arrow' style={arrowStyle}></div>
             {isOpen && <>
                 <h2>{capitalizeWord(part)}</h2>
                 {settingsForParts[part].map(prop => 
